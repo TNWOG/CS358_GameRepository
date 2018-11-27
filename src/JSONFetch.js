6ae20b25fd8b08fe94aeb6f3867c1f9a8846301e
Ryan Vanden Boomen <theonlyboomer@gmail.com>
2018-11-27 13:06:07 -0600

    Added Local Fetch Feature

---
+++ src/JSONFetch.js
@@ -0,0 +1,26 @@
+class JSONFetch{
+    constructor(divId){
+        this.fileInput = document.createElement("input")
+        this.fileInput.setAttribute('id', 'JSONFetch')
+        this.fileInput.setAttribute('type', 'file')
+        this.fileInput.setAttribute('style', 'display: none;')
+        var element = document.getElementById(divId)
+        element.appendChild(this.fileInput)
+        this.file = ""
+        this.JSONResult = "err"
+    }
+
+    selectJSON(){
+        this.fileInput.click();
+    }
+
+    processJSON(_callback) {
+        var reader = new FileReader()
+        reader.readAsText(this.fileInput.files[0])
+        this.file = reader.result;
+        reader.onload = function(e){
+            this.file = reader.result
+            _callback(jQuery.parseJSON(this.file))
+        }
+    }
+}
